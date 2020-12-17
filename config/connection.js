fcab360de10d830908b2ae7592fcd2a369319f14
chickey49 <camslens@gmail.com>
2020-12-17 10:52:24 -0800

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 
-const mysql = require("mysql");
-// const dbConfig = require("../config/db.config.js");
+var mysql = require('mysql');
 
-var connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Password!',
-    database: 'burgers_db'
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'password!',
+        database: 'burgers_db'
+    })
+};
+
 
 module.exports = connection;
